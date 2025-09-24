85c79d97a7b481db7e78fd5ca4c0c6960ac69e7b
adriel-oloko <adrieloloko6@gmail.com>
2025-09-24 08:35:41 +0100

    Refactor reusable components and enhance homepage layout
    - Updated `reUsable.tsx` to improve component structure and styling.
    - Added `GenerateCards` component with conditional rendering for new features.
    - Enhanced `ScrollBar` and `DesktopNavButton` for better accessibility and usability.
    - Refactored `index.tsx` to include new icons and improved layout for the homepage.
    - Implemented toggle functionality for displaying additional generate options.
    - Updated global styles in `globals.css` for consistency and improved dark mode support.

--- src/components/reUsable.tsx
+++ src/components/reUsable.tsx
@@ -1,71 +1,177 @@
 import Image from "next/image";
 
 type DesktopNavButtonProps = {
-    icon: string;
-    text: string;
+	icon: string;
+	text: string;
 };
 
 type scrollBarProps = {
-    curr: number | null;
-}
+	curr: number | null;
+};
 
 type generateCardsProp = {
-    image: string | null;
-    title: string | null;
-    text: string | null;
-    isNew: boolean;
+	image: string | null;
+	title: string | null;
+	text: string | null;
+	isNew: boolean;
+};
+
+export function OpenButton() {
+	return (
+		<button className="bg-primary-150 dark:bg-primary-800 ml-auto flex h-9 w-22 items-center justify-center rounded-full text-xs font-medium text-black dark:text-white">
+			Open
+		</button>
+	);
 }
 
-export function OpenButton () {
-    return (
-        <button className="bg-primary-150 dark:bg-primary-800 ml-auto flex h-9 w-22 items-center justify-center rounded-full text-xs font-medium text-black dark:text-white">Open</button>
-    )
+export function DesktopNavButton({ icon, text }: DesktopNavButtonProps) {
+	const focusToggle = (e: React.MouseEvent<HTMLElement>) => {
+		e.preventDefault();
+		e.currentTarget.classList.add("bg-white");
+	};
+
+	return (
+		<button
+			title={icon}
+			onClick={(e) => focusToggle(e)}
+			type="button"
+			className="focus:bg-white bg-white p-4 w-fit">
+			<Image
+				src={`/icons/${icon}`}
+				className="size-7"
+				width={22}
+				height={22}
+				alt=""
+			/>
+			{text}
+		</button>
+	);
 }
 
-export function DesktopNavButton ({icon, text}: DesktopNavButtonProps) {
-    const focusToggle = (e: React.MouseEvent<HTMLElement>) => {
-        e.preventDefault();
-        e.currentTarget.classList.add('bg-white')
-    }
-
-    return (
-        <button title={icon} onClick={ (e) => focusToggle(e)} type="button" className="focus:bg-white bg-white p-4 w-fit">
-            <Image src={`/icons/${icon}`} className="size-7" width={22} height={22} alt="" />
-            { text }
-        </button>
-    )
+export function ScrollBar({ curr }: scrollBarProps) {
+	return (
+		<div className="w-fit mx-auto z-50 flex gap-2">
+			{[1, 2, 3, 4, 5, 6, 7, 8].map((e, key) => {
+				return (
+					<>
+						{e === curr ? (
+							<span
+								key={key}
+								className="bg-primary-850 dark:bg-primary-850 size-2 rounded-full inline-block"></span>
+						) : (
+							<span
+								key={key}
+								className="bg-primary-150 dark:bg-primary-0 size-2 rounded-full inline-block"></span>
+						)}
+					</>
+				);
+			})}
+		</div>
+	);
 }
 
+export function GenerateCards({
+	image,
+	title,
+	text,
+	isNew,
+}: generateCardsProp) {
+	return (
+		<div className="genCards flex justify-between items-end h-fit">
+			<div className="flex gap-x-2 w-3/4 h-fit items-center">
+				{image == "train" ? (
+					<svg
+						width="42"
+						height="42"
+						viewBox="0 0 24 24"
+						fill="none"
+						className="dark:text-primary-0 text-[#c4c4c5] size-12 shrink-0"
+						xmlns="http://www.w3.org/2000/svg">
+						<defs>
+							<clipPath id="circleMask">
+								<circle cx="12" cy="12" r="7"></circle>
+							</clipPath>
+							<linearGradient
+								id="fadeGradient"
+								x1="0%"
+								y1="0%"
+								x2="0%"
+								y2="100%">
+								<stop
+									offset="0%"
+									stop-color="white"
+									stop-opacity="0.6"></stop>
+								<stop
+									offset="75%"
+									stop-color="white"
+									stop-opacity="0.15"></stop>
+								<stop
+									offset="100%"
+									stop-color="white"
+									stop-opacity="0"></stop>
+							</linearGradient>
+						</defs>
+						<image
+							href="https://www.krea.ai/train-icon-photo.webp"
+							x="5"
+							y="5"
+							width="14"
+							height="14"
+							clip-path="url(#circleMask)"></image>
+						<circle
+							cx="12"
+							cy="12"
+							r="6"
+							fill="url(#fadeGradient)"></circle>
+						<path
+							d="M16.625 20.1226C14.6815 21.2446 12.3964 21.6219 10.1954 21.1841C7.99441 20.7463 6.02762 19.5232 4.66148 17.7429C3.29534 15.9625 2.62303 13.7462 2.7698 11.5068C2.91658 9.26752 3.87243 7.15792 5.45926 5.57108"
+							stroke="currentColor"
+							strokeWidth="1.5"
+							strokeLinecap="round"></path>
+						<path
+							d="M20.0107 7.48682C21.0289 9.25025 21.4366 11.3004 21.1709 13.3192C20.9051 15.338 19.9806 17.2127 18.5407 18.6526"
+							stroke="currentColor"
+							strokeWidth="1.5"
+							strokeLinecap="round"></path>
+						<path
+							d="M12 2.86182C13.2147 2.86182 14.4176 3.10107 15.5398 3.56593C16.6621 4.03079 17.6818 4.71214 18.5407 5.57108"
+							stroke="currentColor"
+							strokeWidth="1.5"
+							strokeLinecap="round"></path>
+						<path
+							d="M7.375 4.10108C8.07507 3.6969 8.8251 3.38622 9.60592 3.177"
+							stroke="currentColor"
+							strokeWidth="1.5"
+							strokeLinecap="round"></path>
+					</svg>
+				) : (
+					<Image
+						src={`/generate-icons/${image}`}
+						className="size-12 aspect-square rounded-[10px] p-2"
+						width={84}
+						height={84}
+						alt=""
+					/>
+				)}
 
-export function ScrollBar ({ curr }: scrollBarProps) {
+				<div className="">
+					<h3 className="font-semibold flex gap-x-2 items-center dark:text-white">
+						{title}
+						{isNew && (
+							<span className="bg-blue-500 pt-1.75 pb-1 text-white rounded-full px-2.75 text-xs font-normal">
+								New
+							</span>
+						)}
+					</h3>
+					<p className="mt-2 text-[#a5a5a5] font-medium leading-tight dark:text-white">
+						{text}
+					</p>
+				</div>
+			</div>
 
-    return (
-        <div className="w-fit mx-auto z-50 flex gap-2">
-            { [1,2,3,4,5,6,7,8].map((e, key) => {
-                return (
-                    <>
-                    { e === curr? <span key={key} className="bg-primary-850 dark:bg-primary-850 size-2 rounded-full inline-block"></span>:
-                    <span key={key} className="bg-primary-150 dark:bg-primary-0 size-2 rounded-full inline-block"></span> }</>
-                )
-            })}
-        </div>
-    )
-}
-
-
-export function GenerateCards ({ image, title, text, isNew }: generateCardsProp) {
-    return (
-        <div className="genCards flex justify-between items-end h-fit">
-            <div className="flex gap-x-2 w-3/4 h-fit">
-                <Image src={`/generate-icons/${image}`} className="size-12 aspect-square rounded-[10px] p-2" width={84} height={84} alt="" />
-                
-                <div className="">
-                    <h3 className="font-semibold flex gap-x-2 items-center dark:text-white">{ title }{ isNew &&<span className="bg-blue-500 pt-1.75 pb-1 text-white rounded-full px-2.75 text-xs font-normal">New</span> }</h3>
-                    <p className="mt-2 text-[#a5a5a5] font-medium leading-tight dark:text-white">{ text }</p>
-                </div>
-            </div>
-
-            <button className="bg-primary-150 text-black rounded-full px-6 text-sm font-medium pb-1.5 pt-2 h-fit">Open</button>
-        </div>
-    )
+			<button className="bg-primary-150 text-black rounded-full px-6 text-sm font-medium pb-1.5 pt-2 h-fit">
+				Open
+			</button>
+		</div>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,65 +2,254 @@
 import Header from "@/components/header";
 import HeroSection from "@/components/heroSection";
 import { GenerateCards, ScrollBar } from "@/components/reUsable";
+import {
+	ChevronDownIcon,
+	ChevronLeftIcon,
+	ChevronRightIcon,
+	ChevronUpIcon,
+	CreditCardIcon,
+	Files,
+} from "lucide-react";
+import Link from "next/link";
 import { useRef, useState } from "react";
 
 export default function Home() {
-    const [visible, setVisible] = useState<number | null>(null);
-    
-    const scrollContainer = useRef<HTMLDivElement>(null)
+	const [visible, setVisible] = useState<number | null>(null);
+	const [genShowAll, setGenShowAll] = useState<boolean>(false);
 
-    const nextClick = () => {
-        scrollContainer.current?.scrollBy({
-            left: 100,
-            behavior: 'smooth',
-        })
-    }
+	const scrollContainer = useRef<HTMLDivElement>(null);
 
-    const prevClick = () => {
-        scrollContainer.current?.scrollBy({
-            left: -100,
-            behavior: 'smooth',
-        })
-    }
+	const nextClick = () => {
+		scrollContainer.current?.scrollBy({
+			left: 100,
+			behavior: "smooth",
+		});
+	};
 
-    return (
-        <>
-            <Header  />
-            <section className="px-6 w-full md:px-8 md:pt-36 rounded-2xl bg-white dark:bg-black">
-                <div ref={scrollContainer} className="relative w-full flex gap-8 justify-start overflow-x-auto h-fit snap-x snap-mandatory rounded-2xl">
-                    <HeroSection id={1} image={"/announce-wan-2-2-image.webp"} title="WAN 2.2 Image generation" text="Generate complex images with the brand new and powerful WAN 2.2 model. Exceptional prompt adherence and ultra-realistic textures." height={1067} onVisible={ setVisible } />
-                    <HeroSection id={2} image={"/OSSKreaFlux1_poster.webp"} title="FLUX.1 Krea" text="We're making the weights to our FLUX.1 Krea model open-source. Download and run our model weights, read the technical report, or generate with it in Krea Image." height={1440} onVisible={ setVisible } />
-                    <HeroSection id={3} image={"/quick-action-hold-product.webp"} title="Realtime Video - Open Beta" text="Our ground-breaking Realtime Video is now accessible to all paid plans. Try it in the Realtime tool." height={1067}  onVisible={ setVisible } />
+	const prevClick = () => {
+		scrollContainer.current?.scrollBy({
+			left: -100,
+			behavior: "smooth",
+		});
+	};
 
-                    <HeroSection id={4} image={"/e06d7a87-e1de-4910-9083-7be5986b45f0_512.webp"} title="Seedream 4.0" text="Try the brand new and record-breaking image generation model Seedream 4.0 by ByteDance." height={1440}  onVisible={ setVisible } />
-                    <HeroSection id={5} image={"/3c1008da-78a1-486e-8cb4-60484c37016a.webp"} title="Real-Time Video Generation" text="Announcing Realtime Video. Generate videos in real-time. Fully frame-consistent, controllable video clips." height={1067}  onVisible={ setVisible } />
-                    <HeroSection id={6} image={"/quick-action-remove-text.webp"} title="Introducing Motion Transfer" text="Bring motion into your characters. Upload any image, record a video of yourself, and make your characters smile, talk, and dance. Powered by Runway Act 2." height={1440}  onVisible={ setVisible } />
-                    <HeroSection id={7} image={"/seedream-4-announcement.webp"} title="WAN 2.2 Image generation" text="Generate complex images with the brand new and powerful WAN 2.2 model. Exceptional prompt adherence and ultra-realistic textures." height={1067}  onVisible={ setVisible } />
-                    <HeroSection id={8} image={"/astronautshirt.jpeg"} title="FLUX.1 Krea" text="We're making the weights to our FLUX.1 Krea model open-source. Download and run our model weights, read the technical report, or generate with it in Krea Image." height={1440}  onVisible={ setVisible } />
-                </div>
-            </section>
+	return (
+		<>
+			<Header />
+			<section className="px-6 w-full md:px-8 md:pt-36 rounded-2xl bg-white dark:bg-black">
+				<div
+					ref={scrollContainer}
+					className="relative w-full flex gap-8 justify-start overflow-x-auto h-fit snap-x snap-mandatory rounded-2xl">
+					<HeroSection
+						id={1}
+						image={"/announce-wan-2-2-image.webp"}
+						title="WAN 2.2 Image generation"
+						text="Generate complex images with the brand new and powerful WAN 2.2 model. Exceptional prompt adherence and ultra-realistic textures."
+						height={1067}
+						onVisible={setVisible}
+					/>
+					<HeroSection
+						id={2}
+						image={"/OSSKreaFlux1_poster.webp"}
+						title="FLUX.1 Krea"
+						text="We're making the weights to our FLUX.1 Krea model open-source. Download and run our model weights, read the technical report, or generate with it in Krea Image."
+						height={1440}
+						onVisible={setVisible}
+					/>
+					<HeroSection
+						id={3}
+						image={"/quick-action-hold-product.webp"}
+						title="Realtime Video - Open Beta"
+						text="Our ground-breaking Realtime Video is now accessible to all paid plans. Try it in the Realtime tool."
+						height={1067}
+						onVisible={setVisible}
+					/>
 
-            <div className="relative mt-4">
-                <ScrollBar curr={visible} />
-                <div className="absolute right-12 -top-2 grid grid-cols-2 w-fit gap-2">
-                    <button title="chevron left" onClick={ prevClick } type="button" className="w-fit p-0.5 rounded-full bg-primary-150 text-black"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000" className="translate-x-0.25"><path d="m432-480 156 156q11 11 11 28t-11 28q-11 11-28 11t-28-11L348-452q-6-6-8.5-13t-2.5-15q0-8 2.5-15t8.5-13l184-184q11-11 28-11t28 11q11 11 11 28t-11 28L432-480Z"/></svg></button>
-                    <button title="chevron rigt" onClick={ nextClick } type="button" className="w-fit p-0.5 rounded-full bg-primary-150 text-black"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000" className="translate-x-0.25"><path d="M504-480 348-636q-11-11-11-28t11-28q11-11 28-11t28 11l184 184q6 6 8.5 13t2.5 15q0 8-2.5 15t-8.5 13L404-268q-11 11-28 11t-28-11q-11-11-11-28t11-28l156-156Z"/></svg></button>
-                </div>
-            </div>
+					<HeroSection
+						id={4}
+						image={"/e06d7a87-e1de-4910-9083-7be5986b45f0_512.webp"}
+						title="Seedream 4.0"
+						text="Try the brand new and record-breaking image generation model Seedream 4.0 by ByteDance."
+						height={1440}
+						onVisible={setVisible}
+					/>
+					<HeroSection
+						id={5}
+						image={"/3c1008da-78a1-486e-8cb4-60484c37016a.webp"}
+						title="Real-Time Video Generation"
+						text="Announcing Realtime Video. Generate videos in real-time. Fully frame-consistent, controllable video clips."
+						height={1067}
+						onVisible={setVisible}
+					/>
+					<HeroSection
+						id={6}
+						image={"/quick-action-remove-text.webp"}
+						title="Introducing Motion Transfer"
+						text="Bring motion into your characters. Upload any image, record a video of yourself, and make your characters smile, talk, and dance. Powered by Runway Act 2."
+						height={1440}
+						onVisible={setVisible}
+					/>
+					<HeroSection
+						id={7}
+						image={"/seedream-4-announcement.webp"}
+						title="WAN 2.2 Image generation"
+						text="Generate complex images with the brand new and powerful WAN 2.2 model. Exceptional prompt adherence and ultra-realistic textures."
+						height={1067}
+						onVisible={setVisible}
+					/>
+					<HeroSection
+						id={8}
+						image={"/astronautshirt.jpeg"}
+						title="FLUX.1 Krea"
+						text="We're making the weights to our FLUX.1 Krea model open-source. Download and run our model weights, read the technical report, or generate with it in Krea Image."
+						height={1440}
+						onVisible={setVisible}
+					/>
+				</div>
+			</section>
 
-            <section className="pb-12 relative px-4 md:px-8 mt-24">
-                <h2 className="text-2xl text-black font-semibold dark:text-white mb-8">Generate</h2>
+			<div className="relative mt-4">
+				<ScrollBar curr={visible} />
+				<div className="absolute right-12 -top-2 grid grid-cols-2 w-fit gap-2">
+					<button
+						title="chevron left"
+						onClick={prevClick}
+						type="button"
+						className="w-fit p-0.75 rounded-full bg-primary-150 text-black">
+						<ChevronLeftIcon size={20} />
+					</button>
+					<button
+						title="chevron right"
+						onClick={nextClick}
+						type="button"
+						className="w-fit p-0.75 rounded-full bg-primary-150 text-black">
+						<ChevronRightIcon size={20} />
+					</button>
+				</div>
+			</div>
 
-                <div className="grid gap-6 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
-                    <GenerateCards image={"image.svg"} title={"Image"} text={"Generate images with custom styles in flux and ideogram"} isNew={true} />
-                    <GenerateCards image={"video.svg"} title={"Video"} text={"Generate images with custom styles in flux and ideogram"} isNew={false} />
-                    <GenerateCards image={"realtime.svg"} title={"Realtime"} text={"Generate images with custom styles in flux and ideogram"} isNew={false} />
-                    <GenerateCards image={"enhancer.svg"} title={"Enhancer"} text={"Generate images with custom styles in flux and ideogram"} isNew={true} />
-                    <GenerateCards image={"edit.svg"} title={"Edit"} text={"Generate images with custom styles in flux and ideogram"} isNew={true} />
-                    <GenerateCards image={"video-lipsync.svg"} title={"Video Lipsync"} text={"Generate images with custom styles in flux and ideogram"} isNew={true} />
-                    <GenerateCards image={"motion-transfer.svg"} title={"Motion Transfer"} text={"Generate images with custom styles in flux and ideogram"} isNew={true} />
-                </div>
-            </section>
-        </>
-    );
+			<section className="flex flex-col gap-8 mt-24">
+				<section className="pb-12 relative px-4 md:px-8">
+					<div className="flex justify-between items-center mb-8">
+						<h2 className="text-2xl text-black font-semibold dark:text-white">
+							Generate
+						</h2>
+						<button
+							onClick={() => setGenShowAll((prev) => !prev)}
+							className="text-blue-500 font-medium flex gap-1">
+							{genShowAll ? (
+								<>
+									<ChevronUpIcon size={24} />
+									Show less
+								</>
+							) : (
+								<>
+									<ChevronDownIcon size={24} />
+									Show all
+								</>
+							)}
+						</button>
+					</div>
+
+					<div className="grid gap-6 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
+						<GenerateCards
+							image={"image.svg"}
+							title={"Image"}
+							text={
+								"Generate images with custom styles in flux and ideogram"
+							}
+							isNew={true}
+						/>
+						<GenerateCards
+							image={"video.svg"}
+							title={"Video"}
+							text={
+								"Generate images with custom styles in flux and ideogram"
+							}
+							isNew={false}
+						/>
+						<GenerateCards
+							image={"realtime.svg"}
+							title={"Realtime"}
+							text={
+								"Generate images with custom styles in flux and ideogram"
+							}
+							isNew={false}
+						/>
+						{genShowAll && (
+							<>
+								<GenerateCards
+									image={"enhancer.svg"}
+									title={"Enhancer"}
+									text={
+										"Generate images with custom styles in flux and ideogram"
+									}
+									isNew={true}
+								/>
+								<GenerateCards
+									image={"edit.svg"}
+									title={"Edit"}
+									text={
+										"Generate images with custom styles in flux and ideogram"
+									}
+									isNew={true}
+								/>
+								<GenerateCards
+									image={"video-lipsync.svg"}
+									title={"Video Lipsync"}
+									text={
+										"Generate images with custom styles in flux and ideogram"
+									}
+									isNew={true}
+								/>
+								<GenerateCards
+									image={"motion-transfer.svg"}
+									title={"Motion Transfer"}
+									text={
+										"Generate images with custom styles in flux and ideogram"
+									}
+									isNew={true}
+								/>
+								<GenerateCards
+									image={"train"}
+									title={"Train"}
+									text={
+										"Generate images with custom styles in flux and ideogram"
+									}
+									isNew={true}
+								/>
+							</>
+						)}
+					</div>
+				</section>
+
+				<section className="pb-12 relative px-4 md:px-8">
+					<div className="flex justify-between items-center mb-8">
+						<h2 className="text-2xl text-black font-semibold dark:text-white">
+							Gallery
+						</h2>
+						<button className="text-blue-500 font-medium flex gap-1">
+							<ChevronDownIcon size={24} />
+							Show all
+						</button>
+					</div>
+				</section>
+			</section>
+
+			<section className="flex gap-x-4 w-full justify-end text-sm font-medium px-4 md:px-8 pb-12">
+				<Link
+					href={"#"}
+					className="flex gap-x-2 items-center bg-primary-100 rounded-full px-4 py-2">
+					<Files className="size-4.5" />
+					Legal
+				</Link>
+				<Link
+					href={"#"}
+					className="flex gap-x-2 items-center bg-primary-100 rounded-full px-4 py-2">
+					<CreditCardIcon className="size-4.5" />
+					Pricing
+				</Link>
+			</section>
+		</>
+	);
 }
